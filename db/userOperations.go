d1648b33dc274e0c0c006d3a7ead4b29b2fdf712
kr4dd <kraddchina@gmail.com>
2022-07-13 15:58:57 +0200

    improved error handling for user operations

--- db/userOperations.go
+++ db/userOperations.go
@@ -15,18 +15,23 @@ var (
 )
 
 //Seal message into a file
-func SealMsg(msg string, filePathDestination string, additionalKey []byte) {
+func SealMsg(msg, filePathDestination string, additionalKey []byte) {
 	fmt.Println("[+] Trying to seal data...")
-	var masterKey []byte
+	
+	var masterKey, sealData []byte
+
 	if additionalKey == nil {
 		//Read master key
-		masterKey = readMasterKey()
+		masterKey, err = readMasterKey()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed reading masterKey while sealing: %v\n", err)
+		}
+
 	} else {
 		masterKey = additionalKey
 	}
 
 	//Cipher msg with a master key
-	var sealData []byte
 	if sealData, err = ecrypto.SealWithProductKey([]byte(msg), masterKey); err != nil {
 		log.Fatal(err)
 	}
@@ -48,51 +53,65 @@ func SealMsg(msg string, filePathDestination string, additionalKey []byte) {
 func UnsealMsgShow(filePathDestination string) {
 	fmt.Println("[+] Trying to unseal data...")
 
-	fmt.Println("Unsealed data: \n" + string(readBytesFromUnsealedFile(readMasterKey(), filePathDestination)))
+	mK, err := readMasterKey()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed reading masterKey while unsealing: %v\n", err)
+	}
+	r, err := readBytesFromUnsealedFile(mK, filePathDestination)
+	fmt.Println("Unsealed data: \n" + string(r))
 }
 
-func getUnsealMsg(masterKey []byte, filePathDestination string) string {
-	return string(readBytesFromUnsealedFile(masterKey, filePathDestination))
+func getUnsealMsg(masterKey []byte, filePathDestination string) (string, error) {
+	r, err := readBytesFromUnsealedFile(masterKey, filePathDestination)
+	if err != nil {
+		return "", fmt.Errorf("Error getting unseal msg: %v", err)
+	}
+
+	return string(r), nil
 }
 
-func readBytesFromUnsealedFile(masterKey []byte, filePathDestination string) []byte {
-	var err error
-
+func readBytesFromUnsealedFile(masterKey []byte, filePathDestination string) ([]byte, error) {
 	//Read bytes from file
-	var sealedData []byte
+	var sealedData, unsealData []byte
 	if sealedData, err = ioutil.ReadFile(filePathDestination); err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("Reading unsealed file failed: %v\n", err)
+
 	}
 
-	var unsealData []byte
 	if unsealData, err = ecrypto.Unseal(sealedData, masterKey); err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("Reading unsealed file failed: %v\n", err)
 	}
 
-	return unsealData
+	return unsealData, nil
 }
 
 //Add info to a previously file
-func AppendMsg(msg string, filePathDestination string) {
+func AppendMsg(msg, filePathDestination string) {
 	fmt.Println("[+] Trying to append data...")
 
 	//Read master key for unseal file
-	masterKey := readMasterKey()
+	masterKey, err := readMasterKey()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed reading masterKey while append: %v\n", err)
+	}
 
-	newMsg := getUnsealMsg(masterKey, filePathDestination) + "\n" + msg
+	newMsg, err := getUnsealMsg(masterKey, filePathDestination)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	SealMsg(newMsg, filePathDestination, masterKey)
+	SealMsg(newMsg + "\n" + msg, filePathDestination, masterKey)
 	fmt.Println("[+] Data was appended and Seal ...")
 
 }
 
-func readMasterKey() []byte {
+func readMasterKey() ([]byte, error) {
 	fmt.Println("\nInsert your masterKey: ")
 
 	maskedPassword, err := gopass.GetPasswdMasked()
 	if err != nil {
-		panic("Input masterKey failed")
+		return nil, fmt.Errorf("Reading masterKey failed: %v\n", err)
 	}
 
-	return []byte(maskedPassword)
+	return []byte(maskedPassword), nil
 }
--- server/server.go
+++ server/server.go
@@ -1,163 +1,183 @@
-package server
+package main
 
 import (
 	"bytes"
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
-	"encoding/pem"
+	"encoding/base64"
+	"encoding/binary"
+	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"math/big"
-	"net"
 	"net/http"
-	"net/http/httptest"
-	"strings"
 	"time"
+
+	"github.com/edgelesssys/ego/enclave"
 )
 
 func main() {
-		// get our ca and server certificate
-		serverTLSConf, clientTLSConf, err := certsetup()
-		if err != nil {
-			panic(err)
-		}
-	
-		// set up the httptest.Server using our certificate signed by our CA
-		server := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintln(w, "success!")
-		}))
-		server.TLS = serverTLSConf
-		server.StartTLS()
-		defer server.Close()
-	
-		// communicate with the server using an http.Client configured to trust our CA
-		transport := &http.Transport{
-			TLSClientConfig: clientTLSConf,
-		}
-		http := http.Client{
-			Transport: transport,
-		}
-		resp, err := http.Get(server.URL)
-		if err != nil {
-			panic(err)
-		}
-	
-		// verify the response
-		respBodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic(err)
-		}
-		body := strings.TrimSpace(string(respBodyBytes[:]))
-		if body == "success!" {
-			fmt.Println(body)
-		} else {
-			panic("not successful!")
-		}
+	// create the server certificate and the servers
+	cert, privKey := createServerCertificate()
+	attestServer := newAttestServer(cert, privKey)
+	secureServer := newSecureServer(cert, privKey)
+
+	// run the servers
+
+	go func() {
+		err := attestServer.ListenAndServe()
+		panic(err)
+	}()
+
+	fmt.Println("listening ...")
+	err := secureServer.ListenAndServeTLS("", "")
+	fmt.Println(err)
 }
 
-func certsetup() (serverTLSConf *tls.Config, clientTLSConf *tls.Config, err error) {
-	// set up our CA certificate
-	ca := &x509.Certificate{
-		SerialNumber: big.NewInt(2019),
-		Subject: pkix.Name{
-			Organization:  []string{"Gradiant"},
-			Country:       []string{"ES"},
-			Province:      []string{""},
-			Locality:      []string{"Pontevedra, Vigo"},
-			StreetAddress: []string{"Estrada do Vilar"},
-			PostalCode:    []string{"36214"},
+func newAttestServer(cert []byte, privKey crypto.PrivateKey) *http.Server {
+	certHash := sha256.Sum256(cert)
+	mux := http.NewServeMux()
+
+	// Returns the server certificate.
+	mux.HandleFunc("/cert", func(w http.ResponseWriter, r *http.Request) { w.Write(cert) })
+
+	// Returns a local report including the server certificate's hash for the given target report.
+	mux.HandleFunc("/report", func(w http.ResponseWriter, r *http.Request) {
+		targetReport := getQueryArg(w, r, "target")
+		if targetReport == nil {
+			return
+		}
+		report, err := enclave.GetLocalReport(certHash[:], targetReport)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("GetLocalReport: %v", err), http.StatusInternalServerError)
+			return
+		}
+		w.Write(report)
+	})
+
+	// Returns a client certificate for the given pubkey.
+	// The given report ensures that only verified enclaves can get certificates for their pubkeys.
+	mux.HandleFunc("/client", func(w http.ResponseWriter, r *http.Request) {
+		pubKey := getQueryArg(w, r, "pubkey")
+		if pubKey == nil {
+			return
+		}
+		report := getQueryArg(w, r, "report")
+		if report == nil {
+			return
+		}
+		if err := verifyReport(report, pubKey); err != nil {
+			http.Error(w, fmt.Sprintf("verifyReport: %v", err), http.StatusBadRequest)
+			return
+		}
+		w.Write(createClientCertificate(pubKey, cert, privKey))
+	})
+
+	return &http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+}
+
+func newSecureServer(cert []byte, privKey crypto.PrivateKey) *http.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) { io.WriteString(w, "pong") })
+
+	// use server certificate also as client CA
+	parsedCert, _ := x509.ParseCertificate(cert)
+	clientCAs := x509.NewCertPool()
+	clientCAs.AddCert(parsedCert)
+
+	return &http.Server{
+		Addr:    "localhost:8081",
+		Handler: mux,
+		TLSConfig: &tls.Config{
+			Certificates: []tls.Certificate{
+				{
+					Certificate: [][]byte{cert},
+					PrivateKey:  privKey,
+				},
+			},
+			ClientAuth: tls.RequireAndVerifyClientCert,
+			ClientCAs:  clientCAs,
 		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(10, 0, 0),
-		IsCA:                  true,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+	}
+}
+
+func verifyReport(reportBytes []byte, pubKey []byte) error {
+	report, err := enclave.VerifyLocalReport(reportBytes)
+	if err != nil {
+		return err
+	}
+	hash := sha256.Sum256(pubKey)
+	if !bytes.Equal(report.Data[:len(hash)], hash[:]) {
+		return errors.New("report data doesn't match the client public key's hash")
+	}
+
+	// We expect the other enclave to be signed with the same key.
+
+	selfReport, err := enclave.GetSelfReport()
+	if err != nil {
+		return err
+	}
+
+	if !bytes.Equal(report.SignerID, selfReport.SignerID) {
+		return errors.New("invalid signer")
+	}
+	if binary.LittleEndian.Uint16(report.ProductID) != 3 {
+		return errors.New("invalid product")
+	}
+	if report.SecurityVersion < 1 {
+		return errors.New("invalid security version")
+	}
+	if report.Debug && !selfReport.Debug {
+		return errors.New("other party is a debug enclave")
+	}
+
+	return nil
+}
+
+func createServerCertificate() ([]byte, crypto.PrivateKey) {
+	template := &x509.Certificate{
+		SerialNumber:          &big.Int{},
+		Subject:               pkix.Name{CommonName: "server"},
+		NotAfter:              time.Now().Add(time.Hour),
 		BasicConstraintsValid: true,
+		IsCA:                  true,
+		DNSNames:              []string{"localhost"},
 	}
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+	cert, _ := x509.CreateCertificate(rand.Reader, template, template, &priv.PublicKey, priv)
+	return cert, priv
+}
 
-	// create our private and public key
-	caPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+func createClientCertificate(pubKey []byte, signerCert []byte, signerPrivKey crypto.PrivateKey) []byte {
+	template := &x509.Certificate{
+		SerialNumber: &big.Int{},
+		Subject:      pkix.Name{CommonName: "client"},
+		NotAfter:     time.Now().Add(time.Hour),
+	}
+	parsedPubKey, _ := x509.ParsePKCS1PublicKey(pubKey)
+	parsedSignerCert, _ := x509.ParseCertificate(signerCert)
+	cert, _ := x509.CreateCertificate(rand.Reader, template, parsedSignerCert, parsedPubKey, signerPrivKey)
+	return cert
+}
+
+func getQueryArg(w http.ResponseWriter, r *http.Request, name string) []byte {
+	values := r.URL.Query()[name]
+	if len(values) == 0 {
+		http.Error(w, fmt.Sprintf("query argument not found: %v", name), http.StatusBadRequest)
+		return nil
+	}
+	result, err := base64.URLEncoding.DecodeString(values[0])
 	if err != nil {
-		return nil, nil, err
+		http.Error(w, fmt.Sprintf("decoding query argument '%v' failed: %v", name, err), http.StatusBadRequest)
+		return nil
 	}
-
-	// create the CA
-	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivKey.PublicKey, caPrivKey)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// pem encode
-	caPEM := new(bytes.Buffer)
-	pem.Encode(caPEM, &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: caBytes,
-	})
-
-	caPrivKeyPEM := new(bytes.Buffer)
-	pem.Encode(caPrivKeyPEM, &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(caPrivKey),
-	})
-
-	// set up our server certificate
-	cert := &x509.Certificate{
-		SerialNumber: big.NewInt(2019),
-		Subject: pkix.Name{
-			Organization:  []string{"Company, INC."},
-			Country:       []string{"US"},
-			Province:      []string{""},
-			Locality:      []string{"San Francisco"},
-			StreetAddress: []string{"Golden Gate Bridge"},
-			PostalCode:    []string{"94016"},
-		},
-		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().AddDate(10, 0, 0),
-		SubjectKeyId: []byte{1, 2, 3, 4, 6},
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:     x509.KeyUsageDigitalSignature,
-	}
-
-	certPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	certBytes, err := x509.CreateCertificate(rand.Reader, cert, ca, &certPrivKey.PublicKey, caPrivKey)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	certPEM := new(bytes.Buffer)
-	pem.Encode(certPEM, &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: certBytes,
-	})
-
-	certPrivKeyPEM := new(bytes.Buffer)
-	pem.Encode(certPrivKeyPEM, &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(certPrivKey),
-	})
-
-	serverCert, err := tls.X509KeyPair(certPEM.Bytes(), certPrivKeyPEM.Bytes())
-	if err != nil {
-		return nil, nil, err
-	}
-
-	serverTLSConf = &tls.Config{
-		Certificates: []tls.Certificate{serverCert},
-	}
-
-	certpool := x509.NewCertPool()
-	certpool.AppendCertsFromPEM(caPEM.Bytes())
-	clientTLSConf = &tls.Config{
-		RootCAs: certpool,
-	}
-
-	return
+	return result
 }
